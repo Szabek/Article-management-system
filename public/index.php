ca29edcf4d42c731cbd8f8638557dfacda88e986
DChabowski <mrschabpl@gmail.com>
2024-08-21 12:16:12 +0200

    response class added

--- public/index.php
+++ public/index.php
@@ -2,9 +2,14 @@
 
 
 use Szabek\Framework\Http\Request;
+use Szabek\Framework\Http\Response;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-echo 'Hello World!';
+$content = '<h1>Hello World!</h1>';
+
+$response = new Response(content: $content, status: 200, headers: []);
+
+$response->send();
