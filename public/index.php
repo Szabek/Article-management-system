2755aafb92d576aad79e585cde14d435486085ac
DChabowski <mrschabpl@gmail.com>
2024-08-21 12:40:25 +0200

    kernel class added

--- public/index.php
+++ public/index.php
@@ -1,15 +1,16 @@
 <?php declare(strict_types=1);
 
 
+use Szabek\Framework\Http\Kernel;
 use Szabek\Framework\Http\Request;
-use Szabek\Framework\Http\Response;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-$content = '<h1>Hello World!</h1>';
 
-$response = new Response(content: $content, status: 200, headers: []);
+$kernel = new Kernel();
+
+$response = $kernel->handle($request);
 
 $response->send();
